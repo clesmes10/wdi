015130918749e85b27199190c95dc7c9b325b084
clesmes10 <37335513+clesmes10@users.noreply.github.com>
2018-04-13 20:49:34 -0500

    Delete alto.py

--- alto.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-#cargar imagen
-imagen = Image.open("imagen.png")
-t = np.linspace(-10,10,20)
-
-def transformadaefedosde(x, y 
-
